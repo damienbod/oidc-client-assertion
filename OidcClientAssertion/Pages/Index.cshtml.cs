eace9fa407556eafbca366f64fe2b9f4a2c542e8
damienbod <damien_bod@hotmail.com>
2025-02-21 20:06:47 +0100

    Update code

--- OidcClientAssertion/Pages/Index.cshtml.cs
+++ OidcClientAssertion/Pages/Index.cshtml.cs
@@ -4,15 +4,7 @@ namespace OidcClientAssertion.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
     public void OnGet()
     {
-
     }
 }
